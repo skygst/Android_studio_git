ac0546a161035fcf578551238db07114d5f64e78
skygst <sky0719zx@163.com>
2017-08-21 18:32:16 +0800

    ti jiao hellow.java

---
+++ ActivityMain.java
@@ -0,0 +1,20 @@
+package com.vondear.tools.activity;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class ActivityMain extends Activity {
+
+    private Context context;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+      
+
+    }
+
+    
+    //==============================================================================================
+}
